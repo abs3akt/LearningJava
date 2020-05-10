0836b805781d2ae5e9b42176a143aef1f58a1a6a
admin <abs3akt@gmail.com>
2020-05-10 20:19:53 +0300

    HelloWorld

--- HelloWorld/src/com/company/Main.java
+++ HelloWorld/src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        helloWorld();
+    }
+    public static void helloWorld(){
         Scanner str = new Scanner(System.in);
         System.out.println(str.nextLine());
-
     }
 }
